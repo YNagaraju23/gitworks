54bd70b9627461d6f70034fc36fe26f0d7db52a8
nagarajy <rajudba7@gmail.com>
2025-11-15 23:30:30 +0530

    to test folder

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-# this is a sample page for testing gitworks package
+#constructor
+def decorator_function(original_function):
+    def wrapper_function():
+        print("Wrapper executed this before {}".format(original_function.__name__))
+        return original_function()
+    return wrapper_function
+
+@decorator_function
+def sum_print():
+    a = 5
+    b = 10
+    print(a + b)
+
+sum_print()
+
+
+    
