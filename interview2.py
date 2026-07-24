4bd09416244fe87ffef6191f12604e0db32ef553
nagarajy <rajudba7@gmail.com>
2026-07-24 22:21:53 +0530

    flatten program added

--- interview2.py
+++ interview2.py
@@ -1,1 +1,12 @@
 #flatten list
+lst = [1, 2, [3, 4], [5, 6, [7, 8, [9]]], 10]
+def flatten(data):
+    result=[]
+    for i in data:
+        if isinstance(i,list):
+            result.extend(flatten(i))
+        else:
+            result.append(i)
+    return result
+print(flatten(lst))
+#o/p:[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
