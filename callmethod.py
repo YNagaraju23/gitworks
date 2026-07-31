955a4be68af3b8e2bd895e8e69dee3b7924e57e9
nagarajy <rajudba7@gmail.com>
2026-07-31 16:57:33 +0530

    important methods are added

---
+++ callmethod.py
@@ -0,0 +1,17 @@
+class CallCounter:
+    def __init__(self):
+        pass
+        #self.count = 0
+
+    def __call__(self, increment=1):
+        self.count=0
+        self.count += increment
+        return f"Total calls: {self.count}"
+
+# Instantiate the class
+counter_instance = CallCounter()
+
+# Call the instance directly like a function
+print(counter_instance())   # Output: Total calls: 1
+print(counter_instance(5))  # Output: Total calls: 6
+print(counter_instance())   # Output: Total calls: 7
