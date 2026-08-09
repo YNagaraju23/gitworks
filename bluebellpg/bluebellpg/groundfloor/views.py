3360fc4de58e65dc4c91d121c3ab7bfb87a5afa2
nagarajy <rajudba7@gmail.com>
2026-08-09 20:35:58 +0530

    two django projects added

---
+++ bluebellpg/bluebellpg/groundfloor/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("Hello, Geeks! Welcome to your first Django app.")
+
+# Create your views here.
