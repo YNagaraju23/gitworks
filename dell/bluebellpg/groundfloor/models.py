2592a1724426a8b0d5d68fd21a6f9ca8431f935c
nagarajy <rajudba7@gmail.com>
2026-08-10 12:27:37 +0530

    dell bluebellpg firstfloor models are added

--- dell/bluebellpg/groundfloor/models.py
+++ dell/bluebellpg/groundfloor/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+class createTable(models.Model):
+    name=models.CharField(max_length=20)
+    sno=models.IntegerField()
+    def __str__(self):
+        return self.name
 # Create your models here.
