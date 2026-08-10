ae6b23b9ada9c2c37f400c3748e5184a2fa88657
nagarajy <rajudba7@gmail.com>
2026-08-10 17:52:33 +0530

    display_name with context display added in views.py

--- dell/bluebellpg/groundfloor/urls.py
+++ dell/bluebellpg/groundfloor/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('views/',views.index,name='index'),
+    path('views/',views.index),
     path('date_time/',views.date_time),
     path('show_html/',views.simple_view),
+    path('age_check/',views.age_check),
+    path('display_name/',views.display_name),
 ]
